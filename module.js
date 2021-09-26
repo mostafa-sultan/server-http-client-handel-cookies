c3adf4856ed06aac536a177887c10e2ee09c0740
Mostafa Sultan <58178645+mostafa-sultan@users.noreply.github.com>
2021-09-27 01:30:56 +0200

    improvement

--- module.js
+++ module.js
@@ -1,47 +1,22 @@
-var cookiesStore="";
-var axios = require('axios'); 
-
+var cookiesStore = "";
+var axios = require('axios');
 exports.cookiesStore = cookiesStore;
-exports.sendRequestSync = async (method,url,headers,body) => { 
-   headers["cookie"] = cookiesStore;
-    store="";
-    var config = {
-      method: method,
-      url: url,
-      headers:headers,
-      data : body
-    }; 
-   await axios(config)
-    .then(function (response) {  
-       cookiesStore=response.headers['set-cookie'].toString()+cookiesStore;
-       console.log(cookiesStore) 
-       return cookiesStore;
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
-    return store;
+exports.sendRequest = async (method, url, headers, body) => {
+  headers["cookie"] = cookiesStore;
+  var config = {
+    method: method,
+    url: url,
+    headers: headers,
+    data: body
+  };
+  var res = await axios(config)
+  if (res) {
+    try {
+      cookiesStore = res.headers['set-cookie'].toString() + cookiesStore;
+    } catch (e) { }
+    return config
+  } else {
+    return "";
   }
 
-
-  exports.sendRequest =  (method,url,headers,body) => { 
-    headers["cookie"] = cookiesStore;
-    store="";
-    var config = {
-      method: method,
-      url: url,
-      headers:headers,
-      data : body
-    }; 
-    console.log(config)
-    axios(config)
-    .then(function (response) {  
-       cookiesStore=response.headers['set-cookie'].toString()+cookiesStore;
-    //    console.log(cookiesStore) 
-       return cookiesStore;
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
-    return store;
-  }
+}
